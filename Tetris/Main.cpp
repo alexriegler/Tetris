80f441939bfe3a45e6519779ab09a800e6352870
Alex Riegler <53669754+alexriegler@users.noreply.github.com>
2020-11-05 22:50:21 -0600

    Add rotate function

--- Tetris/Main.cpp
+++ Tetris/Main.cpp
@@ -2,6 +2,25 @@
 
 std::wstring tetromino[7];
 
+/// <summary>
+/// Returns a new index for a tetromino piece given its coordinates and a rotation.
+/// </summary>
+/// <param name="px">The x position of the piece</param>
+/// <param name="py">The y position of the piece</param>
+/// <param name="r">The degree of rotation: 0 = none, 1 = 90, 2 = 180, 3 = 270</param>
+/// <returns>Rotated index</returns>
+int Rotate(int px, int py, int r)
+{
+	// TODO: Use enum for rotation
+	switch (r % 4)
+	{
+	case 0: return py * 4 + px;			//   0 degrees
+	case 1: return 12 + py - (px * 4);	//  90 degrees
+	case 2: return 15 - (py * 4) - px;	// 180 degrees
+	case 3: return 3 - py + (px * 4);	// 270 degrees
+	}
+}
+
 int main()
 {
 	tetromino[0].append(L"..X.");
